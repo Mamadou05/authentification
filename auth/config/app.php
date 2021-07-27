1e85967865b7b4d3b5e8af48a0a29020eadee678
Mamadou05 <50830946+Mamadou05@users.noreply.github.com>
2021-07-27 09:17:22 +0000

    Update app.php
    Correction du fichier d'envoie de mail

--- auth/config/app.php
+++ auth/config/app.php
@@ -13,8 +13,8 @@ return [
         'encrption' => 'tls',
         'port' => 587,
         'host' => 'smtp.gmail.com',
-        'username' => 'soldat.teste@gmail.com',
-        'password' => '71020296',
+        'username' => 'exemple@gmal.com',
+        'password' => '*******',
         'from' => 'no-reply@devscreencast.com',
         'sender_name' => 'User Authentication'
     ],
